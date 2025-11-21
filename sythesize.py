43ec45d213e54571c8fbbfe447f89574c52d23a6
bdogforlina98 <bdogforlina98@gmail.com>
2025-11-21 12:20:58 -0500

    final push

---
+++ sythesize.py
@@ -0,0 +1,24 @@
+import boto3
+
+with open('speech.txt', 'r') as file:
+    text = file.read()
+    print(text)
+
+
+polly = boto3.client('polly')
+
+
+response = polly.synthesize_speech(
+    Engine='generative',
+    Outputformat='mp3',
+    Text=text,
+    VoiceId='Stephen'
+)
+
+
+audioStream = response['AudioStream']
+
+
+with open("example.mp3","wb") as f:
+    f.write(audioStream.read())
+    print("polly output saved.")
